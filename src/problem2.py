17ac18698b0a8aaf869c7acba19bc51ff25c00e5
liuh8 <liuh8@rose-hulman.edu>
2018-12-11 13:55:39 -0500

    problem2

--- src/problem2.py
+++ src/problem2.py
@@ -196,7 +196,7 @@ def problem2b(rect, n, delta, win):
       :type win:    rg.RoseWindow
     """
     # -------------------------------------------------------------------------
-    # TODO: 3. Implement and test this function.
+    # D: 3. Implement and test this function.
     #          Tests have been written for you (above).
     # -------------------------------------------------------------------------
     # -------------------------------------------------------------------------
